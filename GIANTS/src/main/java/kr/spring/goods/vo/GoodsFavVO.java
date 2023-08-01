988337ab7bd60b59243d7f2980328a57747e1d95
user <user@DESKTOP-2KP3VTQ>
2023-08-01 12:04:40 +0900

    feat: GoodsFavVO 업로드

---
+++ GIANTS/src/main/java/kr/spring/goods/vo/GoodsFavVO.java
@@ -0,0 +1,14 @@
+package kr.spring.goods.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GoodsFavVO {
+	private int fav_num; //찜번호
+	private int mem_num; //회원번호
+	private int goods_num; //상품번호
+}
